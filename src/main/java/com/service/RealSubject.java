5db6593da455f5ed873edae4b99e4c1cad80652f
lg <408366905@qq.com>
2024-04-04 21:40:26 +0800

    Revert "算法"
    This reverts commit f89754321f2daed3099d2020be28a331e3c1acab.

--- src/main/java/com/service/RealSubject.java
+++
@@ -1,9 +0,0 @@
-package com.service;
-
-public class RealSubject implements Subject{
-    private final String name = "ligang";
-    @Override
-    public void visit() {
-        System.out.println(name);
-    }
-}
--- src/main/java/com/task/RunnableTask.java
+++
@@ -1,8 +0,0 @@
-package com.task;
-
-public class RunnableTask implements Runnable {
-    @Override
-    public void run() {
-        System.out.println("XXXXX Runnable ...");
-    }
-}
